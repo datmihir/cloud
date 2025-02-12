330f9aa51149aaace7bd4e802470c379e437f271
Mihir <mpusadkar633@gmail.com>
2025-02-12 21:57:27 +0530

    new cloud

--- MyRemoteImpl.java
+++ MyRemoteImpl.java
@@ -2,13 +2,9 @@ import java.rmi.*;
 import java.rmi.server.*;
 
 public class MyRemoteImpl extends UnicastRemoteObject implements MyRemote {
-
-    // Constructor
     MyRemoteImpl() throws RemoteException {
         super();
     }
-
-    // Implementing the remote method
     @Override
     public String getMessage(String name) throws RemoteException {
         if (name.equalsIgnoreCase("Java RMI")) {
--- MyServer.java
+++ MyServer.java
@@ -4,13 +4,8 @@ import java.rmi.registry.*;
 public class MyServer {
     public static void main(String args[]) {
         try {
-            // Create remote object
             MyRemote obj = new MyRemoteImpl();
-
-            // Start RMI Registry on port 2000
             LocateRegistry.createRegistry(2000);
-
-            // Bind the remote object to the registry
             Naming.rebind("rmi://localhost:2000/myservice", obj);
 
             System.out.println("RMI Server is running...");
